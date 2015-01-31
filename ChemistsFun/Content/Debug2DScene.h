80c8f6215fa8d80ef65056a52c4a5f78183ac89d
Blake Sakaluk <blake.sakaluk@gmail.com>
2015-01-31 15:11:48 -0700

    Started work on graphics engine

---
+++ ChemistsFun/Content/Debug2DScene.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "..\Common\DeviceResources.h"
+#include "ShaderStructures.h"
+#include "..\Common\StepTimer.h"
+
+namespace ChemistsFun
+{
+	// This sample renderer instantiates a basic rendering pipeline.
+	class Debug2DScene
+	{
+	public:
+		Debug2DScene(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		void CreateDeviceDependentResources();
+		void CreateWindowSizeDependentResources();
+		void ReleaseDeviceDependentResources();
+
+		void Update(DX::StepTimer const& timer);
+		void Render();
+
+	private:
+		void DrawCircle(float x, float y, float radius);
+
+	private:
+		// Cached pointer to device resources.
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+
+		// Direct2D resources
+		Microsoft::WRL::ComPtr<ID3D11InputLayout>	m_inputLayout;
+		Microsoft::WRL::ComPtr<ID3D11Buffer>		m_vertexBuffer;
+		Microsoft::WRL::ComPtr<ID3D11Buffer>		m_indexBuffer;
+		Microsoft::WRL::ComPtr<ID3D11VertexShader>	m_vertexShader;
+		Microsoft::WRL::ComPtr<ID3D11PixelShader>	m_pixelShader;
+		Microsoft::WRL::ComPtr<ID3D11Buffer>		m_constantBuffer;
+
+		// System resources for cube geometry.
+		ModelViewProjectionConstantBuffer	m_constantBufferData;
+		uint32	m_indexCount;
+
+		// Variables used with the rendering loop.
+		bool	m_loadingComplete;
+	};
+}
+
