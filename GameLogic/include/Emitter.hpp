06355fbb3e97358abcb43c3101dd46a71428e6bc
jesseDtucker <jesseDtucker@gmail.com>
2015-02-01 06:29:35 -0700

    Small improvments to Emitter

--- GameLogic/include/Emitter.hpp
+++ GameLogic/include/Emitter.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <random>
 #include <vector>
 
 #include "PropertyHelper.hpp"
@@ -11,14 +12,24 @@ namespace FluidGame
 	class Emitter
 	{
 	public:
-		Emitter(b2ParticleSystem* particleSystem, b2Vec2 position);
+		Emitter(b2ParticleSystem* particleSystem, 
+				b2Vec2 position, 
+				float particlesPerSecond = 0.5f,
+				b2Vec2 velocityMin = { 0.0f, 0.0f },
+				b2Vec2 velocityMax = { 1.0f, 1.0f } );
 		bool operator==(const Emitter rhs);
 
 		void Step(float dt);
 		PROP_GET(b2Vec2, Position);
+		void SetPosition(float x, float y);
 	private:
 		b2ParticleSystem* m_particleSystem;
 		float m_count;
+		b2Vec2 m_velocityMin;
+		b2Vec2 m_velocityMax;
+		float m_secondsPerParticle;
+		std::uniform_real_distribution<float> m_rands;
+		std::mt19937 m_gen;
 		std::vector<int32_t> m_particleIds;
 	};
 }
--- GameLogic/src/Emitter.cpp
+++ GameLogic/src/Emitter.cpp
@@ -1,26 +1,34 @@
+#include <random>
+
 #include "Box2D/Box2D.h"
 #include "Emitter.hpp"
 
 using namespace std;
 using namespace FluidGame;
 
-Emitter::Emitter(b2ParticleSystem* particleSystem, b2Vec2 position)
+Emitter::Emitter(b2ParticleSystem* particleSystem, b2Vec2 position, float particlesPerSecond, b2Vec2 velocityMin, b2Vec2 velocityMax)
 	: m_particleSystem(particleSystem)
 	, m_count(0.0f)
+	, m_secondsPerParticle(1.0f / particlesPerSecond)
 	, m_Position(position)
+	, m_velocityMin(velocityMin)
+	, m_velocityMax(velocityMax)
+	, m_rands(0.0f, 1.0f)
+	, m_gen(std::random_device()())
 {
-
+	
 }
 
 void Emitter::Step(float dt)
 {
 	m_count += dt;
-	while (m_count > 1.0f)
+	while (m_count > m_secondsPerParticle)
 	{
-		m_count -= 1.0f;
+		m_count -= m_secondsPerParticle;
 		b2ParticleDef particleDef;
 		particleDef.position = m_Position;
-		particleDef.velocity = { 0.01f, 0.01f };
+		particleDef.velocity = { m_velocityMin.x + m_rands(m_gen) * m_velocityMax.x,
+			m_velocityMin.y + m_rands(m_gen) * m_velocityMax.y };
 		auto id = m_particleSystem->CreateParticle(particleDef);
 		m_particleIds.push_back(id);
 	}
